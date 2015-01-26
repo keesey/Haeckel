0312accf355113e79d693ce91950846856d4d97f
T. Michael Keesey <keesey@gmail.com>
2015-01-25 23:51:50 -0800

    Added compression to `PathBuilder`.

--- src/lib/builders/PathBuilder.ts
+++ src/lib/builders/PathBuilder.ts
@@ -1,14 +1,27 @@
 /// <reference path="../interfaces/Builder.ts"/>
 /// <reference path="../interfaces/Point.ts"/>
+/// <reference path="../functions/pt/create.ts"/>
+/// <reference path="../functions/precisionEqual.ts"/>
 module Haeckel
 {
 	export class PathBuilder implements Builder<string>
 	{
 		private points: Point[] = [];
 
-		add(point: Point)
+		close: boolean = true;
+
+		add(point: Point): PathBuilder;
+		add(x: number, y: number): PathBuilder;
+		add(a: any, y?: number): PathBuilder
 		{
-			this.points.push(point);
+			if (typeof a === 'number')
+			{
+				this.points.push(pt.create(<number> a, y));
+			}
+			else
+			{
+				this.points.push(<Point> a);
+			}
 			return this;
 		}
 
@@ -19,8 +32,42 @@ module Haeckel
 			{
 				return 'M0,0Z';
 			}
-			// :TODO: compression?
-			return 'M' + this.points.map((point: Point) => String(point.x) + ' ' + point.y).join('L') + 'Z';
+			var last = this.points[0];
+			var result: string[] = ['M' + last.x + ' ' + last.y];
+			var lastCommand = 'M';
+			for (var i = 1; i < n; ++i)
+			{
+				var point = this.points[i];
+				if (precisionEqual(last.x, point.x))
+				{
+					if (!precisionEqual(last.y, point.y))
+					{
+						if (lastCommand === 'V')
+						{
+							result.pop();
+						}
+						result.push((lastCommand = 'V') + point.y);
+					}
+				}
+				else if (precisionEqual(last.y, point.y))
+				{
+					if (lastCommand === 'H')
+					{
+						result.pop();
+					}
+					result.push((lastCommand = 'H') + point.x);
+				}
+				else
+				{
+					result.push((lastCommand = 'L') + point.x + ' ' + point.y);
+				}
+				last = point;
+			}
+			if (this.close)
+			{
+				result.push('Z');
+			}
+			return result.join('');
 		}
 		
 		reset()
